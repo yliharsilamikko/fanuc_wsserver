699b145d1b36a0e7a183831560a80dd3fee920c1
yliharsilamikko <yliharsilamikko@gmail.com>
2022-02-25 19:39:44 +0200

    commit

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+
+const io = require("socket.io")(server,
+  {
+    cors:{
+      origin:"*"
+    }
+  });
+
+
 const axios = require("axios");
 
 const PORT = process.env.PORT || 3000;
